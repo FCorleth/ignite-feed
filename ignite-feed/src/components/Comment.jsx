840ba3009a041a4390926d6458785a7866f9f2e0
Corleth <151011728+FCorleth@users.noreply.github.com>
2025-08-30 09:28:43 -0300

    feat: Add comment component

---
+++ ignite-feed/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from "./Comment.module.css";
+import { Trash, ThumbsUp } from "phosphor-react";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/fcorleth.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time
+                title="27 de agosto de 2025 às 08:10"
+                dateTime="2025-08-27 08:10:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
